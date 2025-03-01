4b6b7a90fd62ea58d5446bbf637832f1cd4d2b65
kadenpatten <kadenpatten52@gmail.com>
2025-02-28 22:34:23 -0700

    Fix authentication, edit messages and admin_dashboard

--- factory_simulation/simulation/urls.py
+++ factory_simulation/simulation/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.index, name="home"),
     path("shop/", views.shop, name="shop"),
     path("admin-dashboard/", views.admin_dashboard, name="admin_dashboard"),
     path("create-team/", views.create_team, name="create_team"),
